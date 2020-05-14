b72dcff2ab48971b8ab046519029c2bc8aee937e
Larissa Uchôa <ltuchoa@gmail.com>
2020-05-14 09:30:12 -0300

    Drawing lines completed

--- Untangler/ConnectionView.swift
+++ Untangler/ConnectionView.swift
@@ -13,6 +13,7 @@ class ConnectionView: UIView {
     var dragChanged: (() -> Void)?
     var dragFinished: (() -> Void)?
     var touchStartPos = CGPoint.zero
+    var after: ConnectionView!
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
--- Untangler/ConnectionViewController.swift
+++ Untangler/ConnectionViewController.swift
@@ -12,9 +12,21 @@ class ConnectionViewController: UIViewController {
 
     var currentLevel = 0
     var connections = [ConnectionView]()
+    let renderedLines = UIImageView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        renderedLines.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(renderedLines)
+        
+        NSLayoutConstraint.activate([
+            renderedLines.topAnchor.constraint(equalTo: view.topAnchor),
+            renderedLines.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            renderedLines.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            renderedLines.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
         
         overrideUserInterfaceStyle = .dark
         view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -37,9 +49,22 @@ class ConnectionViewController: UIViewController {
             
             connections.append(connection)
             view.addSubview(connection)
+            
+            connection.dragChanged = { [weak self] in
+                self?.redrawLines()
+            }
+        }
+        
+        for i in 0..<connections.count {
+            if i == connections.count - 1 {
+                connections[i].after = connections[0]
+            } else {
+                connections[i].after = connections[i + 1]
+            }
         }
         
         connections.forEach(place(_:))
+        redrawLines()
         
     }
     
@@ -48,6 +73,18 @@ class ConnectionViewController: UIViewController {
         let randomY = CGFloat.random(in: 50...view.bounds.maxY - 50)
         
         connection.center = CGPoint(x: randomX, y: randomY)
+    }
+    
+    func redrawLines() {
+        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        
+        renderedLines.image = renderer.image { ctx in
+            for connection in connections {
+                UIColor.white.set()
+                ctx.cgContext.setLineWidth(5)
+                ctx.cgContext.strokeLineSegments(between: [connection.after.center, connection.center])
+            }
+        }
     }
 
 
